cccf4865cc5bfe768c4d523f7e1c0cdbb6264fac
Eric P <ericpp@gmail.com>
2026-02-26 21:04:25 -0600

    Disconnect the previous live socket if connecting to a new stream

--- src/lib/Shared/Player/AudioPlayer.svelte
+++ src/lib/Shared/Player/AudioPlayer.svelte
@@ -166,10 +166,17 @@
 
   function startListening(url) {
     console.log("url: ", url);
+
+    // Disconnect existing connection if it exists
+    if ($liveItemSocket && !$liveItemSocket.disconnected) {
+      $liveItemSocket.disconnect();
+    }
+
     $liveItemSocket = undefined;
     $remoteLiveSplits = undefined;
-    // Only create new connection if there isn't already one
-    if (browser && (!$liveItemSocket || $liveItemSocket.disconnected) && url) {
+
+    // Create new connection
+    if (browser && url) {
       $liveItemSocket = io.connect(url);
       $liveItemSocket.on("remoteValue", function (data) {
         $remoteLiveSplits = data;
